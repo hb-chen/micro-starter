70a0725354eb548eb9878d0eb31cc98b03a3cb2b
Hobo86 <hobo.chb@gmail.com>
2017-10-15 20:36:18 +0800

    web服务调试

--- account/web/handler/handler.go
+++ account/web/handler/handler.go
@@ -5,24 +5,28 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/micro/go-log"
 	"github.com/micro/go-micro/client"
-	example "github.com/micro/examples/template/srv/proto/example"
+	user "github.com/hb-go/micro/auth/srv/proto/user"
 
 	"golang.org/x/net/context"
 )
 
 func ExampleCall(w http.ResponseWriter, r *http.Request) {
+	log.Logf("example call")
 	// decode the incoming request as json
 	var request map[string]interface{}
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		log.Logf("example call decode err!")
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	// call the backend service
-	exampleClient := example.NewExampleClient("go.micro.srv.template", client.DefaultClient)
-	rsp, err := exampleClient.Call(context.TODO(), &example.Request{
-		Name: request["name"].(string),
+	userClient := user.NewUserClient("go.micro.srv.auth", client.DefaultClient)
+	rsp,err:= userClient.GetUserLogin(context.TODO(), &user.ReqLogin{
+		Nickname:"Hobo",
+		Pwd:"pwd",
 	})
 	if err != nil {
 		http.Error(w, err.Error(), 500)
@@ -31,12 +35,13 @@ func ExampleCall(w http.ResponseWriter, r *http.Request) {
 
 	// we want to augment the response
 	response := map[string]interface{}{
-		"msg": rsp.Msg,
+		"user": rsp,
 		"ref": time.Now().UnixNano(),
 	}
 
 	// encode and write the response as json
 	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Logf("example call encode err!")
 		http.Error(w, err.Error(), 500)
 		return
 	}
