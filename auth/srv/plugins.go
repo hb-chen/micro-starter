0e60c402a162312dfb868174369347674268463b
Hobo86 <hobo.chb@gmail.com>
2017-11-16 23:44:17 +0800

    将plugin单独import，便于开发和部署环境分离

---
+++ auth/srv/plugins.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/micro/go-plugins/registry/kubernetes"
+	// etcd v3 registry
+	//_ "github.com/micro/go-plugins/registry/etcdv3"
+	// nats transport
+	//_ "github.com/micro/go-plugins/transport/nats"
+	// kafka broker
+	//_ "github.com/micro/go-plugins/broker/kafka"
+)
